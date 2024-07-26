693bc8c2e9349dca2c1fb31696ce7a7f94b06d90
Todd <todd.griffin61@outlook.com>
2024-07-26 09:04:37 +0100

    reduced text size

--- src/renderer/src/components/SettingPanel.tsx
+++ src/renderer/src/components/SettingPanel.tsx
@@ -11,7 +11,7 @@ interface Props {
 export const SettingPanel = ({ title, settingItems }: Props): JSX.Element => {
   return (
     <div className="bg-[#cccccc22] border-[#777777] border-[1px] rounded-[16px] p-2">
-      <h1 className="text-2xl">{title}</h1>
+      <h1 className="text-xl">{title}</h1>
       {settingItems.map((option) => (
         <div className="flex flex-row justify-between align-middle" key={option.description}>
           <h1 className="self-center">{option.description}</h1>
